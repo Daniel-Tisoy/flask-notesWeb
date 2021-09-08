1f138e2b29f0be1167b72b6e7f971fe196d7c199
ltisoy <danieltisoy2001@gmail.com>
2021-09-08 16:58:07 -0500

    foreign key relationships

--- website/models.py
+++ website/models.py
@@ -9,9 +9,16 @@ class Note(db.Model):
     data = db.Column(db.String(10000))
     #func.now is the current dayTime
     date = db.Column(db.DateTime(timezone=True), default=func.now)
+
+    #foreign key
+    #one to many relationship
+    #one user can have many notes
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 class User(db.Model, UserMixin):
     #creating columns
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
     fist_name = db.Column(db.String(150), nullable=False)
+
+    notes = db.relationship('Note')
