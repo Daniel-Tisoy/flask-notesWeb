39abe19643de379db1109bbdd972eb2a0f748bcf
ltisoy <danieltisoy2001@gmail.com>
2021-09-08 19:39:42 -0500

    loging in users

--- website/auth.py
+++ website/auth.py
@@ -12,8 +12,21 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    #print(data)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # check if the information is valid
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in succesfully!', category='success')
+                return redirect(url_for('views.home'))          
+                
+            else:
+                flash('Incorrect password, try again :(', category='error')
+        else:
+            flash('Email does not exist. :(', category='error')
     return render_template('login.html')
 
 
@@ -30,23 +43,26 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-
-        #checking the information is valid
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+        # checking the information is valid
+        elif len(email) < 4:
             flash('Email must be greater than 3 characters', category='error')
-        elif len(first_name)<2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match', category='error')
-        elif len(password1)<8:
+        elif len(password1) < 8:
             flash('password must be at least 8 characters', category='error')
         else:
-            #add user to database
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            # add user to database
+            new_user = User(email=email, first_name=first_name,
+                            password=generate_password_hash(password1, method='sha256'))
 
-            #add to database
+            # add to database
             db.session.add(new_user)
-            #commit
+            # commit
             db.session.commit()
             flash('account created!', category='success')
             return redirect(url_for('views.home'))
